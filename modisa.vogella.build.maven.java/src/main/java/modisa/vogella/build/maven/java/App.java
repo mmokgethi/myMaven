487872b4adcaf2344ffce009d7cbda2f7a261bba
Modisaotsile Mokgethi <modisamokgethi@gmail.com>
2019-02-20 17:51:05 +0200

    Simple Sum

---
+++ modisa.vogella.build.maven.java/src/main/java/modisa/vogella/build/maven/java/App.java
@@ -0,0 +1,19 @@
+package modisa.vogella.build.maven.java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        int a = 2;
+        int b = 3;
+        int sum;
+
+        sum = a + b;
+
+        System.out.println( "Sum = " + sum );
+    }
+}
